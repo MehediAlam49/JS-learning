e7520ac412c4e0d04221d267f6a3b63b985f40df
MehediAlam49 <mehedialam806@gmail.com>
2025-01-08 19:09:03 +0600

    solve sum of two number and subtract of two number problem using function

---
+++ hw_jan-8.js
@@ -0,0 +1,15 @@
+// user define function
+
+// Sum of two number
+function sumOfTwoNumber(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+console.log(sumOfTwoNumber(5, 6));
+
+// Subtraction of two number
+function subtractionOfTwoNumber(num1, num2) {
+    let subtraction = num1 - num2;
+    return subtraction;
+}
+console.log(subtractionOfTwoNumber(6, 5));
