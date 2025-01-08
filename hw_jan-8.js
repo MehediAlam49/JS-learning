00f7bf61da372d234c65dea6de5610111b7a6279
MehediAlam49 <mehedialam806@gmail.com>
2025-01-08 19:36:23 +0600

    solve LCM and GCD of two number problem

--- hw_jan-8.js
+++ hw_jan-8.js
@@ -5,7 +5,7 @@ function sumOfTwoNumber(num1, num2) {
     let sum = num1 + num2;
     return sum;
 }
-console.log(sumOfTwoNumber(5, 6));
+console.log(`Sum of ${6} and ${5} is: ${sumOfTwoNumber(6, 5)}`);
 
 // Subtraction of two number
 function subtractionOfTwoNumber(num1, num2) {
@@ -13,3 +13,22 @@ function subtractionOfTwoNumber(num1, num2) {
     return subtraction;
 }
 console.log(subtractionOfTwoNumber(6, 5));
+
+// LCM of two number
+function lcmOfTwoNumber(num1, num2) {
+    let lcm = (num1 * num2) / (gcdOfTwoNumber(num1, num2));
+    return lcm;
+}
+console.log(lcmOfTwoNumber(6, 5));
+
+// GCD of two number
+function gcdOfTwoNumber(num1, num2) {
+    let gcd;
+    for (let i = 1; i <= num1 && i <= num2; i++) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            gcd = i;
+        }
+    }
+    return gcd;
+}
+console.log(gcdOfTwoNumber(6, 5));
